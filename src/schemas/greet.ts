29739ae4144ffb933d8aadede2e54313fd70e253
Roa <roamakhtoub@gmail.com>
2026-07-27 13:50:19 +0300

    Initial project skeleton

---
+++ src/schemas/greet.ts
@@ -0,0 +1,8 @@
+import { z } from "zod/v4";
+
+export const greetInputSchema = z.object({
+  name: z
+    .string()
+    .min(1)
+    .describe("The person's first name or preferred name to greet"),
+});
