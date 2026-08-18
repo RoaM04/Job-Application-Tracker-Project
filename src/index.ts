c4e11820977089b4028c0bc962700a833714b614
Roa <roamakhtoub@gmail.com>
2026-08-18 18:03:24 +0300

    Update MCP server index and ignore local startup script

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { registerListJobApplicationsTool } from "./tools/list-job-applications.j
 import { registerGetApplicationStatisticsTool } from "./tools/get-application-statistics.js";
 import { registerUpdateJobApplicationDetailsTool } from "./tools/update-job-application-details.js";
 import { registerGetJobApplicationDetailsTool } from "./tools/get-job-application-details.js";
+import { registerRemoveJobApplicationNoteTool } from "./tools/remove-job-application-note.js";
 
 /**
  * Factory used by stdio so every connection gets a fresh server.
@@ -30,6 +31,7 @@ function createServer(): McpServer {
   registerGetApplicationStatisticsTool(server);
   registerUpdateJobApplicationDetailsTool(server);
   registerGetJobApplicationDetailsTool(server);
+  registerRemoveJobApplicationNoteTool(server);
 
   return server;
 }
